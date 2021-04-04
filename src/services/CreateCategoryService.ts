3123065309bfd603e294cff5221f2117b3e300f3
raulfranck <raulengfranck@gmail.com>
2021-04-03 23:56:29 -0300

    Solid pt2

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,4 +1,5 @@
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
 
 interface IRequest {
     name: string;
@@ -13,9 +14,8 @@ interface IRequest {
 */
 
 class CreateCategoryService {
-    constructor(private categoriesRepository: CategoriesRepository) {
+    constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    }
     execute({name, description}: IRequest): void {
         
         const categoryAlreadyExists = this.categoriesRepository.findByName(name);
