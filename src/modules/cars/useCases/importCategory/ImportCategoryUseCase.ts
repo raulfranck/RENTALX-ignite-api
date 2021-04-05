00592f1ff9cd06e5276c0fc655b68055e7a88917
raulfranck <raulengfranck@gmail.com>
2021-04-05 19:38:10 -0300

    Conceito de stream com FS

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,9 +1,17 @@
-
+import fs from "fs";
+import csvParse from "csv-parse";
 
 class ImportCategoryUseCase {
+    execute(file: Express.Multer.File): void {
+        const stream = fs.createReadStream(file.path);
 
-    execute(file: any) {
-        console.log(file);
+        const parseFile = csvParse();
+
+        stream.pipe(parseFile);
+
+        parseFile.on("data", async (line) => {
+            console.log(line)
+        })
     }
 }
 
